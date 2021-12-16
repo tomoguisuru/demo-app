c5e146f8c2832ffdec5cdbb8496734bd8cf3cb1e
Brandon Drake <brandon.drake@edgecast.com>
2021-12-16 07:51:29 -0500

    Initial commit

--- app/app.js
+++ app/app.js
@@ -3,10 +3,19 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'demo-app/config/environment';
 
+import {
+  provideFASTDesignSystem,
+  fastCard,
+  fastButton,
+  fastTextField,
+} from '@microsoft/fast-components';
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
+
+provideFASTDesignSystem().register(fastCard(), fastButton(), fastTextField());
 
 loadInitializers(App, config.modulePrefix);
